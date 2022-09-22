8aa74458c51eceb8e43e02a0e2325d07bda9ef1b
moe1932 <mhamze@ualberta.ca>
2022-09-22 14:25:24 -0600

    mkn

--- js/secondary.js
+++ js/secondary.js
@@ -98,3 +98,22 @@
     });
     
 })(jQuery);
+
+// smooth scroll 2
+$(document).ready(function(){
+    $(".header-voip .btn").on('click', function(event) {
+
+        if (this.hash !== "") {
+
+            event.preventDefault();
+
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+                window.location.hash = hash;
+            });
+        } 
+    });
+});
